cf7e41efdd8accfa4e86a307266711dcc5463e16
Vesi <veselin.dimitrov@cirvianum.cat>
2023-02-08 12:24:49 +0100

    prova

---
+++ ExamenVicent/src/examenvicent/ExamenVicent.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examenvicent;
+
+/**
+ *
+ * @author veselin.dimitrov
+ */
+public class ExamenVicent {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }
+    
+}
